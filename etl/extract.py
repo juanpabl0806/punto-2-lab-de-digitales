c56593bb814e2babff1fae3fe36854f0b6dae60f
Juan Pablo Pedraza Contreras <juanpedrazac@usantotomas.edu.co>
2025-11-22 01:23:32 -0500

    Update extract.py

--- etl/extract.py
+++ etl/extract.py
@@ -1,12 +1,7 @@
 import pandas as pd
-from .utils import load_config
 
 def extract():
-    cfg = load_config()
-    base = cfg["paths"]["raw_dir"]
-    df_pac = pd.read_csv(f'{base}/{cfg["files"]["pacientes"]}')
-    df_eval = pd.read_csv(f'{base}/{cfg["files"]["evaluaciones"]}', parse_dates=["fecha"])
-    df_erg = pd.read_csv(f'{base}/{cfg["files"]["ergonomia"]}')
-    df_emg = pd.read_csv(f'{base}/{cfg["files"]["emg"]}')
-    df_hab = pd.read_csv(f'{base}/{cfg["files"]["hábitos"]}')
-    return df_pac, df_eval, df_erg, df_emg, df_hab
+    # Leer los CSV generados en data/raw
+    df_pac = pd.read_csv("data/raw/pacientes.csv")
+    df_eval = pd.read_csv("data/raw/evaluaciones_clinicas.csv", parse_dates=["fecha"])
+    return df_pac, df_eval
